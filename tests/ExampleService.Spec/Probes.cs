5cc733fc569ee98f030ee208ebaad21ed3b9ac42
zabronax <zabronax@gmail.com>
2025-12-11 16:47:56 +0100

    feat(spec): add readyness specification

--- tests/ExampleService.Spec/Probes.cs
+++ tests/ExampleService.Spec/Probes.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using System.Net;
+using DotNet.Testcontainers.Builders;
+using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace ExampleService.Spec.Probes;
 
@@ -13,5 +15,44 @@ public class HealthCheckProbe(WebApplicationFactory<Program> factory) : TestEnvi
 
         // Then the response is successful
         response.EnsureSuccessStatusCode();
+    }
+}
+
+public class ReadinessProbe(WebApplicationFactory<Program> factory) : TestEnvironment(factory)
+{
+    [Fact(Skip = "Skipping test until readiness check is implemented")]
+    public async Task GivenNoDatabaseAvailable_WhenTheReadinessCheckIsCalled_ThenTheResponseIsUnsuccessful()
+    {
+        // Given no database available
+        // When the readiness check is called
+        var response = await ServiceHttpClient.GetAsync("/ready");
+
+        // Then the response is unsuccessful
+        Assert.Equal(HttpStatusCode.ServiceUnavailable, response.StatusCode);
+    }
+
+    [Fact(Skip = "Skipping test until readiness check is implemented")]
+    public async Task GivenAHealthyDatabase_WhenTheReadinessCheckIsCalled_ThenTheResponseIsSuccessful()
+    {
+        // Given a healthy database
+        var testContainer = new ContainerBuilder()
+            .WithImage("postgres:16")
+            .WithName("test-postgres")
+            .WithPortBinding(5432)
+            .WithEnvironment("POSTGRES_PASSWORD", "postgres")
+            .WithEnvironment("POSTGRES_USER", "postgres")
+            .WithEnvironment("POSTGRES_DB", "postgres")
+            .WithWaitStrategy(Wait
+                .ForUnixContainer()
+                .UntilHttpRequestIsSucceeded(request =>
+                    request.ForPort(5432).ForStatusCode(HttpStatusCode.OK)))
+            .Build();
+        await testContainer.StartAsync();
+
+        // When the readiness check is called
+        var response = await ServiceHttpClient.GetAsync("/ready");
+
+        // Then the response is successful
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
     }
 }
