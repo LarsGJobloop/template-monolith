786659f2dfd01df14faa434bf550dec33e702394
zabronax <zabronax@gmail.com>
2025-12-11 17:22:41 +0100

    refactor: update test environment
    Updated the test environment to accomodate postgres dependency

--- tests/ExampleService.Spec/Probes.cs
+++ tests/ExampleService.Spec/Probes.cs
@@ -1,32 +1,29 @@
 ﻿using System.Net;
-using DotNet.Testcontainers.Builders;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Testcontainers.PostgreSql;
 
 namespace ExampleService.Spec.Probes;
 
-public class HealthCheckProbe(WebApplicationFactory<Program> factory) : TestEnvironment(factory)
+public class HealthCheckProbe : TestEnvironment
 {
     [Fact]
     public async Task GivenAHealhtyService_WhenTheHealthCheckIsCalled_ThenTheResponseIsSuccessful()
     {
         // Given a healthy service
         // When the health check is called
-        var response = await ServiceHttpClient.GetAsync("/health");
+        var response = await Client.GetAsync("/health");
 
         // Then the response is successful
         response.EnsureSuccessStatusCode();
     }
 }
 
-public class ReadinessProbe(WebApplicationFactory<Program> factory) : TestEnvironment(factory)
+public class ReadinessProbe : TestEnvironment
 {
     [Fact(Skip = "Skipping test until readiness spec is implemented")]
     public async Task GivenNoDatabaseAvailable_WhenTheReadinessCheckIsCalled_ThenTheResponseIsUnsuccessful()
     {
         // Given no database available
         // When the readiness check is called
-        var response = await ServiceHttpClient.GetAsync("/ready");
+        var response = await Client.GetAsync("/ready");
 
         // Then the response is unsuccessful
         Assert.Equal(HttpStatusCode.ServiceUnavailable, response.StatusCode);
@@ -35,16 +32,11 @@ public class ReadinessProbe(WebApplicationFactory<Program> factory) : TestEnviro
     [Fact(Skip = "Skipping test until readiness spec is implemented")]
     public async Task GivenAHealthyDatabase_WhenTheReadinessCheckIsCalled_ThenTheResponseIsSuccessful()
     {
-        // Given a healthy database
-        var postgres = new PostgreSqlBuilder()
-            .WithDatabase("postgres")
-            .WithUsername("postgres")
-            .WithPassword("postgres")
-            .Build();
-        await postgres.StartAsync();
+        // Given a healthy database is available
+        await InitializeAsync();
 
         // When the readiness check is called
-        var response = await ServiceHttpClient.GetAsync("/ready");
+        var response = await Client.GetAsync("/ready");
 
         // Then the response is successful
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
--- tests/ExampleService.Spec/TestEnvironment.cs
+++ tests/ExampleService.Spec/TestEnvironment.cs
@@ -1,10 +1,48 @@
 using Microsoft.AspNetCore.Mvc.Testing;
+using Testcontainers.PostgreSql;
 
 namespace ExampleService.Spec;
 
-public class TestEnvironment(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
+static class TestEnvironmentVariables
 {
-    protected readonly WebApplicationFactory<Program> Factory = factory;
+    public const string DB_USER = "default_user";
+    public const string DB_PASSWORD = "default_password";
+    public const string DB_DATABASE = "default_database";
+}
 
-    public HttpClient ServiceHttpClient => Factory.CreateClient();
+public class TestEnvironment : IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _postgres;
+
+    public WebApplicationFactory<Program> Factory { get; private set; }
+
+    public HttpClient Client => Factory.CreateClient();
+
+    public TestEnvironment()
+    {
+        Factory = new WebApplicationFactory<Program>();
+
+        _postgres = new PostgreSqlBuilder()
+            .WithDatabase(TestEnvironmentVariables.DB_DATABASE)
+            .WithUsername(TestEnvironmentVariables.DB_USER)
+            .WithPassword(TestEnvironmentVariables.DB_PASSWORD)
+            .Build();
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _postgres.StartAsync();
+
+        // Set environment variables for the SUT
+        Environment.SetEnvironmentVariable("DB_HOST", _postgres.Hostname);
+        Environment.SetEnvironmentVariable("DB_PORT", _postgres.GetMappedPublicPort(5432).ToString());
+        Environment.SetEnvironmentVariable("DB_USER", TestEnvironmentVariables.DB_USER);
+        Environment.SetEnvironmentVariable("DB_PASSWORD", TestEnvironmentVariables.DB_PASSWORD);
+        Environment.SetEnvironmentVariable("DB_DATABASE", TestEnvironmentVariables.DB_DATABASE);
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _postgres.DisposeAsync();
+    }
 }
